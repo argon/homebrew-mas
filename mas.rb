cfed8367f41788a93d02575b4543fb8c883401e8
Andrew Naylor <andy@dice.fm>
2015-12-31 01:50:41 +0000

    Revert "Update `mas` to v1.0.1"
    This reverts commit cde3c4904619eff25bbf9f743dc8525a7120cc3b.

--- mas.rb
+++ mas.rb
@@ -1,9 +1,9 @@
 class Mas < Formula
   desc "Mac App Store command line interface"
   homepage "https://github.com/argon/mas"
-  url "https://github.com/argon/mas/releases/download/v1.0.1/mas-cli.zip"
-  version "1.0.1"
-  sha256 "483d2f7424e147436704c22fc2ba3fb5e34782065417194b3847727a803f8204"
+  url "https://github.com/argon/mas/releases/download/v1.0.0/mas-cli.zip"
+  version "1.0"
+  sha256 "85bb39e1f07908ed6f6d46a7e0005ea416a6587a30bb72f8eded76eead76bb57"
 
   def install
     bin.install "mas"
