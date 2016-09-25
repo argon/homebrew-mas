eb8e71f83306dd929ba502cfa9af30db1d602725
Andrew Naylor <argon@mkbot.net>
2016-09-25 23:06:51 +0100

    Update to v1.3.1

--- mas.rb
+++ mas.rb
@@ -1,9 +1,9 @@
 class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/argon/mas"
-  url "https://github.com/argon/mas/releases/download/v1.3.0/mas-cli.zip"
-  version "1.3.0"
-  sha256 "4fee03d76003145df419cb86556f26067770b0e9253f61f3a4d2396a2ee20655"
+  url "https://github.com/argon/mas/releases/download/v1.3.1/mas-cli.zip"
+  version "1.3.1"
+  sha256 "ff4e63dd70e0aa46cbfbe19692049d741d9c5cc28339ed406d06a699fa49e5de"
 
   depends_on :macos => :yosemite
 
